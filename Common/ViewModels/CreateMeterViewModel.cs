701e7e950530a4c522c4910f78272373933f92f9
Wallisson Lopes Guevara <wallissonguevara122@outlook.com>
2025-05-19 22:53:39 -0300

    Add Meter Endpoint

---
+++ Common/ViewModels/CreateMeterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using MeterAPI.Models;
+
+namespace MeterAPI.Common.ViewModels;
+
+public class CreateMeterViewModel : Notifiable<Notification>
+{
+    public required string Name { get; set; }
+    public required string Document { get; set; }
+    public required string Adress { get; set; }
+    public required string Phone { get; set; }
+    public required string Email { get; set; }
+
+    public Client MapTo()
+    {
+        Contract<Notification> contract = ValidateCostumerData();
+
+        if (!string.IsNullOrEmpty(Email)) contract.IsEmail(Email, "Email", "Email inválido");
+
+        AddNotifications(contract);
+
+        return new Client
+        {
+            Name = Name,
+            Document = Document,
+            Address = Adress,
+            Phone = Phone,
+            Email = Email
+        };
+    }
+
+    private Contract<Notification> ValidateCostumerData() =>
+            new Contract<Notification>()
+                .Requires()
+                .IsNotNullOrEmpty(Name, "Name", "Nome da empresa é obrigatório.")
+                .IsCpfOrCnpj(Document, "Document", "O numero informado não é um CPF ou CNPJ.")
+                .IsNotNullOrEmpty(Adress, "Adress", "O endereço é obrigatório.")
+                .IsNotNullOrEmpty(Phone, "Phone", "O telefone é obrigatório.");
+}
